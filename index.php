9827b152a15f195469e100fdceb0d7fc68b9308c
evanmasterson <evanm95@hotmail.com>
2015-11-27 14:48:08 +0000

    accordion LE, Scroller CM

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
 <!DOCTYPE html>
 <html>
+
 	<head>
+		
+	
 		<title>Find a Future</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="css/bootstrap.css" />
 		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="css/scroller.css" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
 		<script type="text/javascript" href="js/gmaps.js"></script>
+		<script type = "text/javascript" href = "JQuery.js" > </script>
+		<script type = "text/javascript" href = "js/Scroll.js" > </script>
+		<script type="text/javascript" src="engine1/jquery.js"></script>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+		
+		<script>
+		  $(function() {
+		    $( "#accordion" ).accordion();
+		  });
+		</script>
 	</head>
 	<body>
-		<div class="navbar-fixed-top navbar-inverse">
-		<div class="container" id="home">
+		<div class="navbar-static-top navbar-inverse" id="home">
+		<div class="container padding" id="top">
 			<div class="navbar-brand">
 			Find-a-Future
 		    </div>
@@ -28,41 +45,91 @@
 			</div>
 		</div>
 	</div>
-	
-	
-	<div class="container" id="img">
-		<h1> BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE BIG IMAGE HERE</h1>
-	</div>
-	
-	<div class="container" id="locator">
+
+	<div id="cbp-fbscroller" class="cbp-fbscroller">
+	<nav>
+		<!-- <a href="#img1" class="cbp-fbcurrent">Section 1</a>
+		<a href="#img2">Section 2</a> -->
+
+
+	</nav>
+	<section id="img1"></section>
+	<section id="content1">
+		<div class="container" id="locator">
 		<div class="row">
 			<div class="col-md-3" id="search">
-				<div id = "menu"> 
-				
+				<div id="accordion">
+				  <h3>Type</h3>
+				  <div>
+				    <p>
+				    Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
+				    ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
+				    amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+				    odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+				    </p>
+				  </div>
+				  <h3>County</h3>
+				  <div>
+				    <p>
+				    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+				    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
+				    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
+				    suscipit faucibus urna.
+				    </p>
+				  </div>
+				  <h3>Add</h3>
+				  <div>
+				    <p>
+				    Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
+				    Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
+				    ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
+				    lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
+				    </p>
+				    <ul>
+				      <li>List item one</li>
+				      <li>List item two</li>
+				      <li>List item three</li>
+				    </ul>
+				  </div>
+				  <h3>Section 4</h3>
+				  <div>
+				    <p>
+				    Cras dictum. Pellentesque habitant morbi tristique senectus et netus
+				    et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
+				    faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
+				    mauris vel est.
+				    </p>
+				    <p>
+				    Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
+				    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+				    inceptos himenaeos.
+				    </p>
+				  </div>
 				</div>
 								SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
 				SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
 				SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
-				SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
-				SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
-				SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
-				SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
-SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
+				 SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
 			</div>
 			<div class="col-md-6" id="map">
-					MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP
+				
+					MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP 
 			</div>
 			<div class="col-md-3" id="rss">
 					RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED
-			</div>
+			</div> 
 		</div>
 	</div>
-	
-	<div class="container" id="collegeList">
+	</section>
+	<section id="img2"></section>
+	<section id="content2">
+		<div class="container" id="collegeList">
 		<?php include ("xmlTransform.php");?>
-	</div>
-	
-	<div class="container padding" id="contact">
+		</div>
+	</section>
+	<section id="img3"></section>
+	<section id="content3">
+		<div class="container padding" id="contact">
 		<form role="form">
 			<div class="form-group">
 				<h1>CONTACT FORM</h1>
@@ -79,37 +146,53 @@ SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEA
 			</div>
 			<button type="submit" class="btn btn-default">Submit</button>
 		</form>
-	</div>
-	<div class="panel-footer" id="footer">
-		<div class="container">
-			<footer class="text-center">Find-a-Future<br/><a href="#">Back to Top</a></footer>
-		</div>
 	</div>	
+	</section>
+
+	
+	<section id="content4">
+		<div class="panel-footer" id="footer">
+			<div class="container">
+				<footer class="text-center">Find-a-Future<br/>
+						<a href="#home">Back to Top</a><br />
+						<a href="#locator">Locator</a><br/>
+						<a href="#collegeList">College List</a><br/>
+						<a href="#contact">Contact</a>
+				</footer>
+			</div>
+		</div>	
+	</section>
+</div>
+
+	
 		
 		
 	
 	<script src="js/bootstrap.js"></script>
-	<script>
+	<script type="text/javascript">
 	$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-	
-	new GMaps({
-	  div: '#map',
-	  lat: -12.043333,
-	  lng: -77.028333
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }1000
+	    }
+	  });
 	});
+	
+	$(document).ready(function(){
+		var map = new GMaps({
+		  div: "#map",
+		  lat: -12.043333,
+		  lng: -77.028333
+		});
+	});
+	
 	
 	</script>
 	</body>
