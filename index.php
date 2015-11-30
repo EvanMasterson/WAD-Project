67ea0c40e4112c575f4869cbda7b73dfff4c9ff4
evanmasterson <evanm95@hotmail.com>
2015-11-30 00:43:29 +0000

    menu LE

--- index.php
+++ index.php
@@ -56,64 +56,42 @@
 		<div class="container" id="locator">
 		<div class="row">
 			<div class="col-md-3" id="search">
-				<div id="accordion">
-				  <h3>Type</h3>
-				  <div>
-				    <p>
-				    Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer
-				    ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit
-				    amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
-				    odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.
-				    </p>
-				  </div>
-				  <h3>County</h3>
-				  <div>
-				    <p>
-				    Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
-				    purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
-				    velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
-				    suscipit faucibus urna.
-				    </p>
-				  </div>
-				  <h3>Add</h3>
-				  <div>
-				    <p>
-				    Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
-				    Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
-				    ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
-				    lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
-				    </p>
-				    <ul>
-				      <li>List item one</li>
-				      <li>List item two</li>
-				      <li>List item three</li>
-				    </ul>
-				  </div>
-				  <h3>Section 4</h3>
-				  <div>
-				    <p>
-				    Cras dictum. Pellentesque habitant morbi tristique senectus et netus
-				    et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
-				    faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
-				    mauris vel est.
-				    </p>
-				    <p>
-				    Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
-				    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-				    inceptos himenaeos.
-				    </p>
-				  </div>
+				<div id="menu">
+					<form>
+						<h4> Add a College Here!</h4>
+						<br>
+						Name of College: <br>
+						<input type="text" name="cName">
+						<br>
+						<br>
+						Address: <br>
+						<input type="text" name="address">
+						<br>
+						<br>
+						Postcode: <br>
+						<input type="text" name="postcode">
+						<br>
+						<br>
+						Type: <br>
+						<input type="text" name="type">
+						<br>
+						<br>
+						Phone Number: <br>
+						<input type="text" name="pNumber">
+						<br>
+						<br>
+						<input type="submit" name="submit">
+						<br>
+					</form>
+					
 				</div>
-								SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
-				SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
-				SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
-				 SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH SEARCH
 			</div>
 			<div class="col-md-6" id="map">
+					<script rel="javascript" href="js/gmaps.js" ></script>
 					MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP MAP 
 			</div>
 			<div class="col-md-3" id="rss">
-					RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED
+				EED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED RSS FEED
 			</div> 
 		</div>
 	</div>
